61c91d67fbf4498653c3a215ef19873102ca51d4
Danny <danielpg2106@gmail.com>
2023-10-17 12:44:08 -0400

    newone

--- Script.js
+++ Script.js
@@ -1,22 +1,15 @@
-// Example: Add a click event handler for navigation buttons
-document.addEventListener("DOMContentLoaded", function() {
-    const headerButtons = document.querySelectorAll(".header-button");
-    
-    headerButtons.forEach(button => {
-        button.addEventListener("click", function(event) {
-            event.preventDefault(); // Prevent the default link behavior
-            const targetId = button.getAttribute("href").substring(1); // Extract the target section ID
-            scrollToSection(targetId); // Custom function to scroll to the target section
+// Smooth scrolling for navigation links
+document.querySelectorAll('a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const target = document.getElementById(targetId);
+
+        window.scrollTo({
+            top: target.offsetTop,
+            behavior: 'smooth'
         });
     });
 });
-
-function scrollToSection(sectionId) {
-    const section = document.getElementById(sectionId);
-    if (section) {
-        section.scrollIntoView({ behavior: "smooth" });
-    }
-}
-
-// You can add more JavaScript functionality as needed
 
